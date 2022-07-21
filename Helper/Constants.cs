21f57d10da0a933cee7cc65fe2cab81d99d3788c
kerngi <giles.kern@bhp.com>
2022-07-21 17:00:17 +0800

    Add Settings classes

---
+++ Helper/Constants.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GZipTools.Helper
+{
+    public struct Constants
+    {
+        public struct Settings
+        {
+            public const string FileName = "Settings.json";
+        }
+    }
+}
---
+++ Helper/Settings.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GZipTools.Helper
+{
+    public class Settings
+    {
+        public static Model.Settings Read()
+        {
+            string fileName = Constants.Settings.FileName;
+            return null;
+        }
+
+        public static void Write(Model.Settings settings)
+        {
+            
+        }
+    }
+}
